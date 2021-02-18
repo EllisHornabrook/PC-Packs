b64102f8638c2054bbfc8404ebd28e4a6c0ba7a5
Ellis Hornabrook <ellishornabrook@gmail.com>
2021-02-18 18:04:14 +0000

    New working routes for footer

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Router } from "@reach/router";
-// import styles from "./Routes.module.scss";
 import Home from "../Home";
+import ProductPage from "../../components/ProductPage";
+import Terms from "../Terms";
+import PrivacyPolicy from "../PrivacyPolicy";
+import ContactUs from "../ContactUs";
 import NotFound from "../NotFound";
 
 const Routes = () => {
-
     return (
         <Router>
             <Home path="/" />
+            <ProductPage path="/product" />
+            <Terms path="/terms" />
+            <PrivacyPolicy path="/policy" />
+            <ContactUs path="/contact" />
             <NotFound default />
         </Router>
     );
