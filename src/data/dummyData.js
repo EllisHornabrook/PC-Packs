973cca90f457343671ca1abe2118fb71fa5084fc
Ellis Hornabrook <ellishornabrook@gmail.com>
2021-02-22 19:46:59 +0000

    Created dummy data to use for design planning

---
+++ src/data/dummyData.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        itemName: "GeForce RTX 3080 Graphics Card | NVIDIA",
+        itemImg: "",
+        itemPrice: "7.99",
+        itemInfo: "",
+        itemDetails: "",
+        raffleOdds: "1/100",
+        maxTickets: 10,
+        ticketCounter: 0
+    },
+    {
+        itemName: "SD",
+        itemImg: "",
+        itemPrice: "4.99",
+        itemInfo: "",
+        itemDetails: "",
+        raffleOdds: "1/50",
+        maxTickets: 10,
+        ticketCounter: 0
+    },
+    {
+        itemName: "Keyboard",
+        itemImg: "",
+        itemPrice: "1.99",
+        itemInfo: "",
+        itemDetails: "",
+        raffleOdds: "1/90",
+        maxTickets: 10,
+        ticketCounter: 0
+    },
+    {
+        itemName: "Monitor",
+        itemImg: "",
+        itemPrice: "5.99",
+        itemInfo: "",
+        itemDetails: "",
+        raffleOdds: "1/100",
+        maxTickets: 10,
+        ticketCounter: 0
+    }
+];
+
+export default data;
