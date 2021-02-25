7eacf85a8f6bce7c5266d0bafe9c8f6acf6f7e1c
Ellis Hornabrook <ellishornabrook@gmail.com>
2021-02-25 16:46:38 +0000

    Adjusted data to display correctly

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -2,11 +2,10 @@ const data = [
     {
         id: 1,
         itemName: "GeForce RTX 3080 Graphics Card | NVIDIA",
-        itemImg: "https://d2skuhm0vrry40.cloudfront.net/2020/articles/2020-09-16-18-20/nvidia-geforce-rtx-3080-where-to-buy-1600276840810.jpg/EG11/thumbnail/750x422/format/jpg/quality/60",
+        itemImg: "https://d2skuhm0vrry40.cloudfront.net/2020/articles/2020-09-16-18-20/nvidia-geforce-rtx-3080-where-to-buy-1600276840810.jpg/EG1thumbnail/750x422/format/jpg/quality/60",
         itemPrice: "7.99",
-        itemInfo: "",
         itemDetails: "",
-        raffleOdds: "1/100",
+        availableTickets: "100",
         maxTickets: 10,
         ticketCounter: 0
     },
@@ -15,10 +14,9 @@ const data = [
         itemName: "SD",
         itemImg: "",
         itemPrice: "4.99",
-        itemInfo: "",
         itemDetails: "",
-        raffleOdds: "1/50",
-        maxTickets: 10,
+        availableTickets: "50",
+        maxTickets: 2,
         ticketCounter: 0
     },
     {
@@ -26,10 +24,9 @@ const data = [
         itemName: "Keyboard",
         itemImg: "",
         itemPrice: "1.99",
-        itemInfo: "",
         itemDetails: "",
-        raffleOdds: "1/90",
-        maxTickets: 10,
+        availableTickets: "80",
+        maxTickets: 5,
         ticketCounter: 0
     },
     {
@@ -37,9 +34,8 @@ const data = [
         itemName: "Monitor",
         itemImg: "",
         itemPrice: "5.99",
-        itemInfo: "",
         itemDetails: "",
-        raffleOdds: "1/100",
+        availableTickets: "100",
         maxTickets: 10,
         ticketCounter: 0
     }
