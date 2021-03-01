70ec57ab92f05243536194c273e2632d200144fa
Ellis Hornabrook <ellishornabrook@gmail.com>
2021-03-01 17:10:51 +0000

    More data for display use

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -1,7 +1,7 @@
 const data = [
     {
         id: 1,
-        itemName: "GeForce RTX 3080 Graphics Card | NVIDIA",
+        itemName: "GeForce RTX 3080 Graphics Card NVIDIA",
         itemImg: "https://d2skuhm0vrry40.cloudfront.net/2020/articles/2020-09-16-18-20/nvidia-geforce-rtx-3080-where-to-buy-1600276840810.jpg/EG1thumbnail/750x422/format/jpg/quality/60",
         itemPrice: "7.99",
         itemDetails: "The GeForce RTX™ 3080 delivers the ultra performance that gamers crave, powered by Ampere—NVIDIA’s 2nd gen RTX architecture. It’s built with enhanced RT Cores and Tensor Cores, new streaming multiprocessors, and superfast G6X memory for an amazing gaming experience.",
@@ -12,108 +12,59 @@ const data = [
             "Memory Size - 10 GB",
             "Memory Type - GDDR6X"
         ],
-        availableTickets: "100",
+        availableTickets: 100,
         maxTickets: 10,
         ticketCounter: 0
     },
     {
         id: 2,
-        itemName: "SD",
-        itemImg: "",
+        itemName: `SAMSUNG 860 PRO SATA 2.5" SSD 2TB`,
+        itemImg: "https://images.samsung.com/is/image/samsung/uk-860-pro-sata-3-2-5-ssd-mz-76p2t0b-eu-rperspectiveblack-130138433?$684_547_PNG$",
         itemPrice: "4.99",
-        itemDetails: "",
-        itemSpecs: [],
-        availableTickets: "50",
+        itemDetails: "Get industry-leading reliability with the 860 PRO. Powered by the latest V-NAND technology and a robust algorithm-based controller, it readily handles heavy workloads of high-end PCs, workstations and NAS (Network Attached Systems) to give lasting assurance to gamers, IT and creative professionals.",
+        itemSpecs: [
+            "Capacity - 2TB",
+            "Max. Read - 560 MB/s",
+            "Max. Write - 530 MB/s",
+            "Interface - SATA 3.0 (6Gb/s)",
+            "Dimensions - 69.85 x 6.8 x 100mm"
+        ],
+        availableTickets: 50,
         maxTickets: 2,
-        ticketCounter: 0
+        ticketCounter: 50
     },
     {
         id: 3,
-        itemName: "Keyboard",
-        itemImg: "",
+        itemName: "CORSAIR ESSENTIAL GAMING BUNDLE",
+        itemImg: "https://cf.shopee.ph/file/21dc778dc21bec25a2af05f9eac64780",
         itemPrice: "1.99",
-        itemDetails: "",
-        itemSpecs: [],
-        availableTickets: "80",
+        itemDetails: "Everything you need for a great gaming experience including an RGB gaming keyboard, stereo headset, gaming mouse, and mouse pad.",
+        itemSpecs: [
+            "Bundle Includes -",
+            "CORSAIR K55 RGB Gaming Keyboard",
+            "CORSAIR HS50 Stereo Gaming Headset",
+            "CORSAIR HARPOON RGB PRO Gaming Mouse",
+            "MM100 Cloth Mouse Pad"
+        ],
+        availableTickets: 80,
         maxTickets: 5,
         ticketCounter: 0
     },
     {
         id: 4,
-        itemName: "Monitor",
-        itemImg: "",
+        itemName: "ROG STRIX XG27WQ Display",
+        itemImg: "https://dlcdnimgs.asus.com/websites/global/products/o7pd5gzfea5yvhyx/images/bg-header-m.jpg",
         itemPrice: "5.99",
-        itemDetails: "",
-        itemSpecs: [],
-        availableTickets: "100",
-        maxTickets: 10,
-        ticketCounter: 0
-    }
-    ,
-    {
-        id: 2,
-        itemName: "SD",
-        itemImg: "",
-        itemPrice: "4.99",
-        itemDetails: "",
-        itemSpecs: [],
-        availableTickets: "50",
-        maxTickets: 2,
-        ticketCounter: 0
-    },
-    {
-        id: 3,
-        itemName: "Keyboard",
-        itemImg: "",
-        itemPrice: "1.99",
-        itemDetails: "",
-        itemSpecs: [],
-        availableTickets: "80",
-        maxTickets: 5,
-        ticketCounter: 0
-    },
-    {
-        id: 4,
-        itemName: "Monitor",
-        itemImg: "",
-        itemPrice: "5.99",
-        itemDetails: "",
-        itemSpecs: [],
-        availableTickets: "100",
-        maxTickets: 10,
-        ticketCounter: 0
-    }
-    ,
-    {
-        id: 2,
-        itemName: "SD",
-        itemImg: "",
-        itemPrice: "4.99",
-        itemDetails: "",
-        itemSpecs: [],
-        availableTickets: "50",
-        maxTickets: 2,
-        ticketCounter: 0
-    },
-    {
-        id: 3,
-        itemName: "Keyboard",
-        itemImg: "",
-        itemPrice: "1.99",
-        itemDetails: "",
-        itemSpecs: [],
-        availableTickets: "80",
-        maxTickets: 5,
-        ticketCounter: 0
-    },
-    {
-        id: 4,
-        itemName: "Monitor",
-        itemImg: "",
-        itemPrice: "5.99",
-        itemDetails: "",
-        itemSpecs: [],
-        availableTickets: "100",
+        itemDetails: "Features WQHD (2560 x 1440) curved panel that delivers up to 77% more onscreen desktop space than standard Full HD (1920 x 1080) displays. Also, delivers an astounding 3,000:1 contrast ratio.",
+        itemSpecs: [
+            "Panel Size (inch) - 27",
+            "Aspect Ratio - 16:9",
+            "Response Time - 1ms MPRT",
+            "Refresh Rate(max) - 165Hz",
+            "Panel Backlight - LED",
+            "Display Surface - Non-Glare"
+        ],
+        availableTickets: 100,
         maxTickets: 10,
         ticketCounter: 0
     }
