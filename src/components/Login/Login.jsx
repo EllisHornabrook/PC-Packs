0062641c187b90ceaf997a71bd319cfe844c110d
Ellis Hornabrook <ellishornabrook@gmail.com>
2021-03-04 16:13:58 +0000

    Replaced google fontA with google logo

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -21,9 +21,10 @@ const Login = (props) => {
                     <p>Password</p>
                     <input type="password" name="password" placeholder="" onChange={() => {}} required />
                 </div>
-                <button type="submit" className={styles.button}>Sign in</button>
+                <button type="submit" className={styles.formButton}>Sign in</button>
+                <p>― or ―</p>
                 <div className={styles.googleButton} onClick={() => {}}>
-                    <FontAwesomeIcon icon={["fab", "google"]} className={styles.icon} />
+                    <img src='https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA' />
                     <p>Sign in with Google</p>
                 </div>
                 <div className={styles.divider} />
